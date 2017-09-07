b9acdc156d50e5f0ca0eed5c7933a0a326ad667c
JLM2627 <arcal619@gmail.com>
2017-09-07 15:49:13 -0400

    dee

--- labs/javaScript/in-class-click-events/script.js
+++ labs/javaScript/in-class-click-events/script.js
@@ -1,2 +1,21 @@
+//window.onload says dont run any JS until all html and css has loaded
+
 window.onload = function() {
-}
+
+const ButtonOne = Document.querySelector(".ButtonOne")
+ButtonOne.addeventListener("click", function() {
+    alert("You have clicked button one")
+});
+
+const ButtonTwo = Document.querySelector("ButtonTwo")
+ButtonTwo.addeventListener("click", function(){
+    const paragraph = document.getElement("information");
+    paragraph.innerText = "A click event is essentially tying a function (as a callback) to an element and specifying what action needs to happen to trigger or call/invoke that function (callback)."
+
+});
+  const ButtonThree = Document.querySelector("ButtonThree")
+  ButtonThree.addeventListener("click", function() {
+    const remove = document.getElementByClass("Information");
+    element.parentNode.removeChild("p");
+});
+

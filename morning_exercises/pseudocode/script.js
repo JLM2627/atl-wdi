03612293ae804e632fc65cdca28119923397b3d5
JLM2627 <arcal619@gmail.com>
2017-09-07 14:09:32 -0400

    morning exercise 9/7/17

--- morning_exercises/pseudocode/script.js
+++ morning_exercises/pseudocode/script.js
@@ -3,16 +3,32 @@
 // Barrels O' RUM
 
 var barrels = function(small, large, total){
+   let small = 60;
+   let large = (total -(small * smallBarrell)) / large
+ return barrel;
 };
-
+barrels(2,3,825) 
 //*************************
 // Problem 2:
 // Sailing the Seas
 
 var shipFuelCost = function(fuelPrice, milesPerGallon){
-  //your code here
+ 
+    var circumference = 24901;
+    var mpg = miles;
+  
+    var totalGallons = circumference / mpg;
+    var totalCost = totalGallons * fuel;
+  
+    return totalCost;
+  };
 };
 
+// const shipFuelCost = (fuelPrice, milesPerGallon)
+// var earthDistance = 24,901;
+// var gallonUsed = (earthDistance / milesPerGallon);
+// var fuelCost = (gallonUsed / milesPerGallon);
+// return totalCost;
 //*************************
 // Problem 3:
 // GROG
@@ -20,6 +36,22 @@ var shipFuelCost = function(fuelPrice, milesPerGallon){
 var calcFruitJuice = function(a, b, c, d){
   //your code here
 };
+
+var calcFruitJuice = function(costcoGallon, costcoPurity, kirklandGallons,  )
+const totalCostcFruitJuice = costGallon
+// Find the total amount of pure fruit juice (gal)/   
+// Total of Costco pure fruit juice:
+  // percentage pure fruit juice * number of gallons
+//Total of Kirkland pure fruit juice:
+  // percentage pure fruit juice * number of gallons
+//Add Costco pure fruit juice total + Kirkland pure fruit juice total
+//Find the total amount of juice (gal):
+//Total of Costco juice + Total of Kirkland Juice
+//Calculate (total pure fruit juice) / (total juice)
+
+
+
+
 
 //DO NOT EDIT BELOW THIS LINE//
 module.exports = {

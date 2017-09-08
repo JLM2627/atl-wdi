7e27e717fadc48c716bbfa7005a8a441b70ba1e6
JLM2627 <arcal619@gmail.com>
2017-09-08 12:29:08 -0400

    jQuery website skeleton

--- instructor_notes/jQuery/in_class_exercises/scripts/script.js
+++ instructor_notes/jQuery/in_class_exercises/scripts/script.js
@@ -1,11 +1,19 @@
+
 // 1. Use jQuery to fetch all `h1` tags and save them to a variable called $h1
-
+//const $h1 = $( "h1" );
+//console.log($h1)
 // 2. Grab the HTML element that is both an H1 and has an ID of "second"
-
+const second = $( "h1#second" );
 // 3. Select the div with an ID of 'myDiv' and change the text to say "jQuery makes JS easy!"
-
-// 4. Append this image to end of the body. http://www.fillmurray.com/200/200
-
+$( "#myDiv").text("Goodnight World with jQuery");
+//console.log() .text(shows on) vs .html(updates html text)
+//4. Append this image to end of the body. http://www.fillmurray.com/200/200
+ $("#container").append("<p>This is Great!</p>");
+  $("body").append('<img src="http://www.fillmurray.com/200/200"/>');
 // 5. Change the background color of the page to green.
-
+$("body").css("backgroundColor", "green");
 // 6. Remove the div with and ID of 'myDiv'
+// $("#myDiv").remove();
+// alternatively there is one called hide. It is hdden from GUI but shows up on DOM
+ $("#myDiv").hide();
+

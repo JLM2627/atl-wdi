cc1ad8934402dc02e90b5aa61e59471fbfd8e929
JLM2627 <arcal619@gmail.com>
2017-09-20 13:22:59 -0400

    working on pizza toppings

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -7,6 +7,16 @@ var app     = express();
 // assigning 3000 as our port
 var port    = 3000;
 
+app.get("/", (req, res) => {
+    const message = "Welcome to Pizza Express!"
+
+    res.send( message )
+})
+app.get('/topping/:type', function(req, res, next) {
+    let type = (req.query.params);
+        res.send(message);
+});
+
 // tells the server to listen for requests on port 3000
 app.listen(port, function(){
   console.log("==========================")

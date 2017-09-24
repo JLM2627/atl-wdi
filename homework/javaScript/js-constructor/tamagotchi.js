4a9416e942a2beea9f2e0acbbef54f0b6dd43091
JLM2627 <arcal619@gmail.com>
2017-09-24 12:20:49 -0400

    constructor and cry methos created

--- homework/javaScript/js-constructor/tamagotchi.js
+++ homework/javaScript/js-constructor/tamagotchi.js
@@ -1,6 +1,19 @@
 console.log('tamagotchi file is loaded');
 
 //your class declaration here
+class Animals {
+    constructor(){
+        this.foodInTummy = 10;
+        this.rested = 10;
+        this.health = 10;
+    }
+
+    cry(){
+        this.foodIntummy -1;
+        console.log("WaHHHHHHH");
+    }
+}
+
 
 
 //create new Tamagotchis

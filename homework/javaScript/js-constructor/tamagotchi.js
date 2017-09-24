40cbb1fc503fc7b013baafe8436bb5608394d35a
JLM2627 <arcal619@gmail.com>
2017-09-24 12:36:58 -0400

    testing new tamagotchis

--- homework/javaScript/js-constructor/tamagotchi.js
+++ homework/javaScript/js-constructor/tamagotchi.js
@@ -1,7 +1,7 @@
 console.log('tamagotchi file is loaded');
 
 //your class declaration here
-class Animals {
+class Tamagotchi {
     constructor(){
         this.foodInTummy = 10;
         this.rested = 10;
@@ -10,13 +10,18 @@ class Animals {
 
     cry(){
         this.foodIntummy -1;
-        console.log("WaHHHHHHH");
+        console.log('WaHHHHHHH');
+        console.log('Oh my! Amount of food in belly:' this.foodInTummy)
     }
 }
 
 
 
 //create new Tamagotchis
+let tamTam1 = newTamgotchi();
+let tamTam2 = newTamgotchi();
 
 
 //test out your Tamagotchies below via console.logs
+console.log(tamtam1);
+console.log(tamTam2);

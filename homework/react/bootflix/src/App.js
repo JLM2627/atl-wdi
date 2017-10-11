3495bbd19ce790e4e80d65fa5b809cce56d0bcdb
JLM2627 <arcal619@gmail.com>
2017-10-11 08:46:43 -0400

    app.js

--- homework/react/bootflix/src/App.js
+++ homework/react/bootflix/src/App.js
@@ -1,36 +1,62 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Header from './components/Header';
 import Search from './components/Search';
 import Movie from './components/Movie';
 import example from './omdbExample.json'
 
 class App extends Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
-      movie: example
-    }
-  }
-
+      movie: []
+      
+    };
+  } 
+ /*  componentWillMount() {
+    axios.get("http://www.omdbapi.com/apikey") 
+  } */
   //Update these methods to make axios calls to OMDB and update this.state.movie with the response from the server
   searchByTitle = () => {
-    console.log("Search by Title");
-  }
+      axios.get('http://www.omdbapi.com/?apikey=[yourkey]&t=[title]', {
+          params: {
+            apikey: "",
+            t: "title"
+          }
 
-  searchById = () => {
+          })
+          .then((response)=>{
+            const movie = response.data.films;
+            this.setState({movie: movie});
+            
+          })
+          .catch((error)=>{
+            console.log("error", error);
+          });
+      
+    } 
+      
+        
+  
+   
+   
+
+ /*  searchById = () => {
     console.log("Search by ID");
-  }
+  } */
 
   //Pass _searchByTitle, _searchById, and this.state.movie to it's appropriate child components.
   render() {
+    const movie = rthis.state.movies.map(())
     return (
       <div className="App">
         <Header />
         <Search />
         <Movie />
       </div>
-    );
+    )
   }
+  
 }
 
 export default App;
